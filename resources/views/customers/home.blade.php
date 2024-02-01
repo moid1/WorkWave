ded624d55368eb98f775940ba6d1cfb6d208689c
Moid Khan <mueedkhan766@gmail.com>
2024-02-02 02:04:32 +0500

    done

--- resources/views/customers/home.blade.php
+++ resources/views/customers/home.blade.php
@@ -10,16 +10,17 @@
                     <div class="card-body">
                         <h4 class="mt-0 m-b-15 header-title">Recent Orders</h4>
                         <div class="table-responsive">
-                            <table class="table table-hover m-b-0">
+                            <table id="datatable" class="table table-bordered dt-responsive nowrap">
                                 <thead>
                                 <tr>
                                     <th>Order Id</th>
                                     <th>Order Date</th>
-                                    <th>Generator Manifest</th>
-                                    <th>Transporter Manifest</th>
+                                    <th>Manifest</th>
+                                    <th>Load Type</th>
+                                    {{-- <th>Transporter Manifest</th>
                                     <th>Processor Manifest</th>
                                     <th>Disposal Manifest</th>
-                                    <th>Original Manifest</th>
+                                    <th>Original Manifest</th> --}}
                                 </tr>
 
                                 </thead>
@@ -28,12 +29,17 @@
                                         @foreach ($orders as $order)
                                         <tr>
                                             <td>000{{$order->id}}</td>
-                                            <td>000{{$order->created_at}}</td>
-                                            <td><a href="{{url($order->manifest->generator ?? '')}}"> Manifest</a></td>
-                                            <td><a href="{{url($order->manifest->transporter ?? '')}}"> Manifest</a></td>
+                                            <td>{{$order->created_at->format('d M Y')}}</td>
+                                            @if($order->manifest)
+                                                <td><a href="{{url($order->manifest->generator ?? '')}}">Manifest </a></td>
+                                            @else
+                                            <td>N/A</td>
+                                            @endif
+                                            <td>{{$order->load_type}}</td>
+                                            {{-- <td><a href="{{url($order->manifest->transporter ?? '')}}"> Manifest</a></td>
                                             <td><a href="{{url($order->manifest->processor ?? '')}}"> Manifest</a></td>
                                             <td><a href="{{url($order->manifest->disposal ?? '')}}"> Manifest</a></td>
-                                            <td><a href="{{url($order->manifest->original_generator ?? '')}}"> Manifest</a></td>
+                                            <td><a href="{{url($order->manifest->original_generator ?? '')}}"> Manifest</a></td> --}}
                                         </tr>
                                         @endforeach
                                     @endif
