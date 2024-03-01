05db68e7947234fba592ee9f33c89d2cefeb80e2
Moid <moidkhan22@yahoo.com>
2024-03-02 00:29:00 +0500

    done

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -241,6 +241,7 @@
             $('#poc_name').val($(this).data('poc'));
             $('#email').val($(this).data('email'));
             $('#notes').val($(this).data('lastest-note'));
+            $('#loadType').val($(this).data('order-type'));
             // $('#second_poc').val($(this).data('second-poc'));
             // $('#mail_address').val($(this).data('mail-address'));
             // $('#charge_type').val($(this).data('charge-type'));
