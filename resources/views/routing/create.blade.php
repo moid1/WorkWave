2d465a1fea80a5e29f61ce3af5f711a2c61cc9bb
Moid <moidkhan22@yahoo.com>
2024-03-20 05:35:10 +0500

    dnoe

--- resources/views/routing/create.blade.php
+++ resources/views/routing/create.blade.php
@@ -17,63 +17,68 @@
     }
 </style>
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-12">
-        @if (Session::has('success'))
-        <div class="alert alert-success alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                    aria-hidden="true">×</span></button>
-            {{ Session::get('success') }}
-        </div>
-        @elseif(Session::has('error'))
-        <div class="alert alert-danger alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                    aria-hidden="true">×</span></button>
-            {{ Session::get('error') }}
-        </div>
-        @endif
-    </div>
-
-    <div class="col-lg-7">
-        <div class="select-driver">
-            <label for="">Select Driver</label>
-            <select id="driverID" name="" id="" class="js-example-basic-multiple form-control form-select  mb-3">
-                @foreach ($drivers as $driver)
-                <option value="{{ $driver->id }}">{{ $driver->name }}</option>
-                @endforeach
-            </select>
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            @if (Session::has('success'))
+                <div class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                            aria-hidden="true">×</span></button>
+                    {{ Session::get('success') }}
+                </div>
+            @elseif(Session::has('error'))
+                <div class="alert alert-danger alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                            aria-hidden="true">×</span></button>
+                    {{ Session::get('error') }}
+                </div>
+            @endif
         </div>
 
-        <div class="d-flex mt-3 justify-content-between">
-            <div style="">
-                <strong>Date Filter:</strong>
-                <input type="text" name="daterange" value="" />
-                <button class="btn btn-success filter">Filter</button>
+        <div class="col-lg-7">
+            <div class="select-driver">
+                <label for="">Select Driver</label>
+                <select id="driverID" name="" id=""
+                    class="js-example-basic-multiple form-control form-select  mb-3">
+                    @foreach ($drivers as $driver)
+                        <option value="{{ $driver->id }}">{{ $driver->name }}</option>
+                    @endforeach
+                </select>
             </div>
-            <div id="generateRoutes" class="btn btn-primary ">Generate Routes</div>
-        </div>
 
-        <div class="row mt-5">
-            <h4> Route Details </h4>
-            <div class="col-lg-12" id="orderDetailDiv">
+            <div class="d-flex mt-3 justify-content-between">
+                <div style="">
+                    <strong>Date Filter:</strong>
+                    <input type="text" name="daterange" value="" />
+                </div>
+                <div id="generateRoutes" class="btn btn-primary ">Generate Routes</div>
             </div>
+
+            <div class="row mt-5">
+                <h4> Route Details </h4>
+                <div class="col-lg-12" id="orderDetailDiv">
+                </div>
+            </div>
+
+            <div class="row float-right">
+                <button id="createRoute" class="btn btn-primary d-none">Create Route</button>
+            </div>
+
         </div>
 
-    </div>
 
-
-    <div class="col-lg-5">
-        <div id="mymap"></div>
+        <div class="col-lg-5">
+            <div id="mymap"></div>
+        </div>
     </div>
-</div>
 @endsection
 
 @section('pageSpecificJs')
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-<script type="text/javascript">
-    var latlngs = [];
-    var actualResponse = null;
+    <script type="text/javascript">
+        var latlngs = [];
+
+        var actualResponse = null;
         $(function() {
             var startDate = localStorage.getItem('startDate');
             var endDate = localStorage.getItem('endDate');
@@ -93,35 +98,24 @@
             }
 
 
-
-            $(".filter").click(function() {
-                localStorage.setItem('startDate', $('input[name="daterange"]').data('daterangepicker')
-                    .startDate);
-
-                localStorage.setItem('endDate', $('input[name="daterange"]').data('daterangepicker')
-                    .endDate);
-
-                table.draw();
-            });
-
         });
 
         function getDistance(point1, point2) {
-    var lat1 = point1.lat;
-    var lon1 = point1.lng;
-    var lat2 = point2.lat;
-    var lon2 = point2.lng;
+            var lat1 = point1.lat;
+            var lon1 = point1.lng;
+            var lat2 = point2.lat;
+            var lon2 = point2.lng;
 
-    var R = 6371; // Radius of the Earth in km
-    var dLat = (lat2 - lat1) * Math.PI / 180; // Convert degrees to radians
-    var dLon = (lon2 - lon1) * Math.PI / 180; // Convert degrees to radians
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var distance = R * c; // Distance in km
-    return distance;
-}
+            var R = 6371; // Radius of the Earth in km
+            var dLat = (lat2 - lat1) * Math.PI / 180; // Convert degrees to radians
+            var dLon = (lon2 - lon1) * Math.PI / 180; // Convert degrees to radians
+            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+                Math.sin(dLon / 2) * Math.sin(dLon / 2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            var distance = R * c; // Distance in km
+            return distance;
+        }
 
         function geocodeAddress(response, address, index, waypoints) {
             // Geocoder object
@@ -151,45 +145,46 @@
                         })
 
                         if (latlngs.length === response.length) {
-                            
-                            var directionsService = new google.maps.DirectionsService();
-                            var directionsRenderer = new google.maps.DirectionsRenderer({
-                                map: mymap
+
+
+
+
+                            // Calculate distances of each waypoint from the origin
+                            var distances = waypoints.slice(1).map(function(waypoint) {
+                                return getDistance(waypoints[0].location, waypoint.location);
                             });
 
+                            // Create an array of indices and sort it based on distances
+                            var sortedIndices = Array.from(Array(waypoints.length - 1)
+                                .keys()); // Exclude the first waypoint
+                            sortedIndices.sort(function(a, b) {
+                                return distances[b] - distances[
+                                    a]; // Sort from longest to shortest distance
+                            });
 
-                        // Calculate distances of each waypoint from the origin
-                        var distances = waypoints.slice(1).map(function(waypoint) {
-                            return getDistance(waypoints[0].location, waypoint.location);
-                        });
+                            console.log(sortedIndices);
 
-                        // Create an array of indices and sort it based on distances
-                        var sortedIndices = Array.from(Array(waypoints.length - 1).keys()); // Exclude the first waypoint
-                        sortedIndices.sort(function(a, b) {
-                            return distances[b] - distances[a]; // Sort from longest to shortest distance
-                        });
+                            // Reorder waypoints based on sorted indices
+                            var sortedWaypoints = [waypoints[0]]; // Keep the first waypoint unchanged
+                            sortedIndices.forEach(function(index) {
+                                sortedWaypoints.push(waypoints[index + 1]);
+                            });
 
-                        console.log(sortedIndices);
+                            // Construct the request object
+                            var request = {
+                                origin: sortedWaypoints[0].location,
+                                destination: sortedWaypoints[sortedWaypoints.length - 1].location,
+                                waypoints: sortedWaypoints.slice(1, -1),
+                                travelMode: 'DRIVING'
+                            };
+                            sortedIndices.forEach(function(sorIn, index) {
+                                var alphabet = String.fromCharCode(66 + index); // 'A' has ASCII code 65
 
-                        // Reorder waypoints based on sorted indices
-                        var sortedWaypoints = [waypoints[0]]; // Keep the first waypoint unchanged
-                        sortedIndices.forEach(function(index) {
-                            sortedWaypoints.push(waypoints[index + 1]);
-                        });
-
-                        // Construct the request object
-                        var request = {
-                            origin: sortedWaypoints[0].location,
-                            destination: sortedWaypoints[sortedWaypoints.length - 1].location,
-                            waypoints: sortedWaypoints.slice(1, -1),
-                            travelMode: 'DRIVING'
-                        };
-                        sortedIndices.forEach(function(sorIn) {
-                        $('#orderDetailDiv').append(`
+                                $('#orderDetailDiv').append(`
                         <div class="row">
-                            <span class="removeOrder" data-orderid=${response[sorIn].id} >X</span>
+                            <span class="removeOrder mt-2" data-orderid=${response[sorIn].id} >X</span>
                             <div class="col-lg-2">
-                                <p class="border p-2">${response[sorIn].customer.business_name}<p>
+                          Stop ${alphabet} <p class="border p-2">${response[sorIn].customer.business_name}<p>
                             </div>
                             <div class="col-lg-3">
                                 <p class="border p-2 ml-3">000${response[sorIn].id}<p>
@@ -197,32 +192,73 @@
                             <div class="col-lg-3">
                                 <p class="border p-2 ml-3">${response[sorIn].load_type}<p>
                             </div>
-                            <div class="col-lg-2">
-                                <input type="number" min="1" max="${response.length}" class="form-control "/>
+                            <div class="col-lg-2 d-none">
+                                <input type="number" min="1" max="${response.length}" value=${index+1} class="form-control changeOrdering"/>
                             </div>
                         </div>
                         `)
-                    });
-
-                            
-                    $('.removeOrder').on('click', function(){
-                        // Retrieve the removeOrderID
-                        let removeOrderID = parseInt($(this).attr('data-orderid'));
-                        console.log(removeOrderID);
-
-                        // Filter out the object with the specified ID
-                        actualResponse = actualResponse.filter(obj => obj.id !== removeOrderID);
-                        // Remove the parent element of the clicked button
-                       $('#orderDetailDiv').empty()
-                        generateWayPoints(actualResponse);
+                            });
 
 
-                        });
+                            $('.removeOrder').on('click', function() {
+                                // Retrieve the removeOrderID
+                                let removeOrderID = parseInt($(this).attr('data-orderid'));
+                                console.log(removeOrderID);
+
+                                // Filter out the object with the specified ID
+                                actualResponse = actualResponse.filter(obj => obj.id !== removeOrderID);
+
+                                // Clear existing markers and directions
+                                clearWaypoints(directionsRenderer);
+
+                                // Empty the order details container
+                                $('#orderDetailDiv').empty();
+
+                                // Generate waypoints and directions for the updated response
+                                generateWayPoints(actualResponse);
+                            });
+
+                            $('.changeOrdering').on('change', function() {
+                                clearWaypoints(directionsRenderer);
+
+                                // Empty the order details container
+                                $('#orderDetailDiv').empty();
+
+                                // Generate waypoints and directions for the updated response
+                                generateWayPoints(actualResponse);
+                            })
+
 
                             directionsService.route(request, function(response, status) {
                                 if (status == 'OK') {
                                     console.log(response);
                                     directionsRenderer.setDirections(response);
+
+                                    // var route = response.routes[0];
+                                    // var legs = route.legs;
+                                    // for (var i = 0; i < legs.length; i++) {
+                                    //     var markerLabel = (i + 1).toString(); // Using numbers as labels
+                                    //     var markerIcon = {
+                                    //         url: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' +
+                                    //             markerLabel + '|FF0000|000000',
+                                    //         labelOrigin: new google.maps.Point(11,
+                                    //             50) // Position of the label
+                                    //     };
+                                    //     var marker = new google.maps.Marker({
+                                    //         position: legs[i].start_location,
+                                    //         map: mymap, // Assuming 'map' is your Google Maps instance
+                                    //         label: {
+                                    //             text: markerLabel,
+                                    //             color: 'white'
+                                    //         },
+                                    //         icon: markerIcon
+                                    //     });
+                                    // }
+
+                                    $('#createRoute').removeClass('d-none');
+
+
+                                    $('#createRoute').removeClass('d-none')
                                 } else {
                                     window.alert('Directions request failed due to ' + status);
                                 }
@@ -254,6 +290,14 @@
             let startDate = $('input[name="daterange"]').data('daterangepicker').startDate.format('YYYY-MM-DD');
             let endDate = $('input[name="daterange"]').data('daterangepicker').endDate.format('YYYY-MM-DD');
             $('#generateRoutes').on('click', function() {
+
+                localStorage.setItem('startDate', $('input[name="daterange"]').data('daterangepicker')
+                    .startDate);
+
+                localStorage.setItem('endDate', $('input[name="daterange"]').data('daterangepicker')
+                    .endDate);
+
+
                 $('#orderDetailDiv').empty()
                 let driverId = $('#driverID').val();
                 $.ajax({
@@ -265,39 +309,80 @@
                         to_date: endDate
                     },
                     success: function(response) {
+                        if (response.length == 0) {
+                            alert('No Order Found for this driver');
+                            location.reload()
+                        }
                         actualResponse = response;
                         generateWayPoints(response);
-                       
-                        
+
+
                     }
                 })
             });
+
+            $('#createRoute').on('click', function() {
+                var orderIds = "";
+
+                $('.removeOrder').each(function(index) {
+                    // Extract the value of the data-orderid attribute
+                    var orderId = $(this).data('orderid');
+
+                    // Add the order ID to the string, followed by a comma
+                    orderIds += orderId;
+
+                    // Add a comma if it's not the last element
+                    if (index < $('.removeOrder').length - 1) {
+                        orderIds += ",";
+                    }
+                });
+                console.log(orderIds)
+
+            })
         });
 
         var myLatlng = new google.maps.LatLng(30.749860, -98.180590);
         var myOptions = {
             zoom: 20,
             center: myLatlng,
-            mapTypeId: google.maps.MapTypeId.SATELLITE
+            mapTypeId: google.maps.MapTypeId.HYBRID,
+            mapTypeControl: true,
+    mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.HYBRID],
+        style: google.maps.MapTypeControlStyle.DEFAULT,
+        position: google.maps.ControlPosition.TOP_RIGHT
+    }
         }
         var mymap = new google.maps.Map(document.getElementById("mymap"), myOptions);
+        var directionsService = new google.maps.DirectionsService();
+        var directionsRenderer = new google.maps.DirectionsRenderer({
+            map: mymap
+        });
 
-        function generateWayPoints(response){
+        function generateWayPoints(response) {
+
             latlngs = [];
-                var waypoints = [{
+            var waypoints = [{
                 location: {
-                lat: 30.749860,
-                lng: -98.180590
+                    lat: 30.749860,
+                    lng: -98.180590
                 }
-                }];
-                
-                response.forEach(function(order, index) {
+            }];
+
+            response.forEach(function(order, index) {
                 // Do something with each item in the array
                 console.log('index', index);
                 geocodeAddress(response, order.customer.mail_address,
-                index, waypoints);
-                });
-                
-                }
-</script>
+                    index, waypoints);
+            });
+
+        }
+
+        function clearWaypoints(directionsRenderer) {
+            directionsRenderer.setOptions({
+                markers: []
+            });
+
+        }
+    </script>
 @endsection
