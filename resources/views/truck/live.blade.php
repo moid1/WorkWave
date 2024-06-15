22487481934be316e22f7e6c209b09b02291f4b5
TMS <moidzaeem@gmail.com>
2024-06-15 14:46:11 +0500

    doen

--- resources/views/truck/live.blade.php
+++ resources/views/truck/live.blade.php
@@ -113,7 +113,7 @@
                             });
                             if (trailer.trailer_swap_order&& trailer.trailer_swap_order
                                         .trailer_pick_up && trailer.trailer_swap_order.trailer_going !== null && trailer.trailer_swap_order.trailer_going !== undefined && trailer.trailer_swap_order.trailer_going !== '' && trailerCoordinates.hasOwnProperty(trailer.trailer_swap_order.trailer_going)) {
-                                var coordinates = trailerCoordinates[trailer.trailer_going];
+                                var coordinates = trailerCoordinates[trailer.trailer_swap_order.trailer_going];
                                 console.log('thids ', coordinates);
 
                                 var markers = new google.maps.Marker({
