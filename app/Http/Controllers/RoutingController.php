0e6c56423b5bd18b6d7bc0ca9d445814dd65d7e8
TMS <moidzaeem@gmail.com>
2024-06-28 17:31:54 +0500

    done

--- app/Http/Controllers/RoutingController.php
+++ app/Http/Controllers/RoutingController.php
@@ -238,6 +238,7 @@ class RoutingController extends Controller
     public function getDriverOrderRouting(Request $request)
     {
         $truckDriver = TruckDriver::where('truck_id', $request->truck_id)->latest()->first();
+        dd($truckDriver);
 
         if ($truckDriver) {
             $data = Order::where('driver_id', $truckDriver->user_id)
