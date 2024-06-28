ce280576e6d8a6abfb74fc93aba8c65220d32105
TMS <moidzaeem@gmail.com>
2024-06-28 20:39:05 +0500

    done

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -52,7 +52,7 @@ class OrderController extends Controller
                     return 'N/A';
                 })
                 ->editColumn('created_at', function ($row) {
-                    return $row->delivery_date
+                    return $row->delivery_date;
                 })
                 ->editColumn('email', function ($row) {
                     return $row->customer->email;
