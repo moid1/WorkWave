bf02448ee4207a81016c28683104d7bdc467ae97
TMS <moidzaeem@gmail.com>
2024-07-08 14:42:24 +0500

    done

--- app/Http/Controllers/RoutingController.php
+++ app/Http/Controllers/RoutingController.php
@@ -243,6 +243,7 @@ class RoutingController extends Controller
             $data = Order::where('driver_id', $truckDriver->user_id)
                 ->where('is_routed', false)
                 ->with(['customer', 'user', 'driver']);
+                dd($data);
 
             if ($request->filled('from_date') && $request->filled('to_date')) {
                 $fromDate = Carbon::parse($request->from_date);
