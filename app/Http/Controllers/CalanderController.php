61652a700258d1e6deb3cf05729a44e4a2f57ba4
TMS <moidzaeem@gmail.com>
2025-02-12 01:12:16 +0500

    done

--- app/Http/Controllers/CalanderController.php
+++ app/Http/Controllers/CalanderController.php
@@ -46,7 +46,7 @@ class CalanderController extends Controller
             return Carbon::parse($item->routing_date)->englishDayOfWeek;
         })->toArray();
 
-        dd($dataByDay);
+        // dd($dataByDay);
 
         $dataGroupedByTruck = [];
         $englishDaysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -93,7 +93,7 @@ class CalanderController extends Controller
 
         // dd($dataGroupedByTruck);
 
-        // dd($dataGroupedByTruck);
+        dd($dataGroupedByTruck);
         return view('calander.index', compact('dataGroupedByTruck'));
     }
 
