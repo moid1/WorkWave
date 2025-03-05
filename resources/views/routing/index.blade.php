0a103cf610d989f998f6f6e8c4ee3b0fa7d76bee
TMS <moidzaeem@gmail.com>
2025-03-05 20:55:02 +0500

    done

--- resources/views/routing/index.blade.php
+++ resources/views/routing/index.blade.php
@@ -57,7 +57,7 @@
                                             @endphp
                                             <td>
                                                 @foreach ($orders as $order)
-                                                    <a href="{{route('order.show', $order)}}">{{$order}}</a>
+                                                    <a href="{{route('order.show', $order)}}">{{$order}}</a>,
                                                 @endforeach
                                             </td>
                                             <td>{{ $route->truck->name }}</td>
